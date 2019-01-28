f96df2c5f6e20145e6768e89cba275d85299236c
Stephen Cassat <Stephencassat@yahoo.com>
2019-01-28 09:17:31 -0700

    Added node.hpp to model

---
+++ DataStructures/Controller/Controller.cpp
@@ -0,0 +1,14 @@
+//
+//  Controller.cpp
+//  DataStructures
+//
+//  Created by Cassat, Stephen on 1/28/19.
+//  Copyright © 2019 Cassat Stephen. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller :: start()
+{
+    cout << "Welcome to the data Structures App." << endl;
+}
---
+++ DataStructures/Controller/Controller.hpp
@@ -0,0 +1,22 @@
+//
+//  Controller.hpp
+//  DataStructures
+//
+//  Created by Cassat, Stephen on 1/28/19.
+//  Copyright © 2019 Cassat Stephen. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+using namespace std;
+
+class Controller
+{
+public:
+    void start();
+};
+
+#endif /* Controller_hpp */
+
