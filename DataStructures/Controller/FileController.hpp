e9ee05519f593f1caae826b08a48310cabcd6f02
Stephen Cassat <Stephencassat@yahoo.com>
2019-02-07 09:45:14 -0700

    Created LinkedList.hpp in Linear Folder

--- DataStructures/Controller/FileController.hpp
+++ DataStructures/Controller/FileController.hpp
@@ -10,11 +10,20 @@
 #define FileController_hpp
 
 #include <stdio.h>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<sstream>
+#include<string>
+
+#include "../Resources/CrimeData.hpp"
+#include "../Resources/Music.hpp"
+
+
+
+
 
 #endif /* FileController_hpp */
-
-#include<iostream>
-
 
 class FileController
 {
@@ -22,3 +31,41 @@ public:
     static vector<CrimeData> readCrimeDataToVector(string filename);
     static vector<Music> musicDataToVector(string filename);
 };
+
+vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
+{
+    std :: vector<CrimeData> crimeVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    //If the file exists at that path.
+    if(dataFile.is_open())
+    {
+        //Keepreading until you are at the end of the file
+        while(!dataFinel.eof())
+        {
+            //Grab each line from the CSV separated by the carriage return character.
+            getLine(dataFile, currentCSVLine, '\r');
+            //Exclude header row
+            if (rowCount ! = 0)
+            {
+                //Create a CrimeData instance from the line. Exclude a blank line(usually if opened separately
+                if(currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimeVector.push_back(row);
+                }
+            }
+            rowCount++
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    return crimeVector;
+}
+
---
+++ DataStructures/Model/Linear/LinkedList.hpp
@@ -0,0 +1,18 @@
+//
+//  LinkedList.h
+//  DataStructures
+//
+//  Created by Cassat, Stephen on 2/7/19.
+//  Copyright © 2019 Cassat Stephen. All rights reserved.
+//
+
+
+#include "List.hpp"
+
+using namespace std; //Used for keyword access. Use this to maek sure your pointers are pointing to the right things
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+
+#endif /* LinkedList_hpp */
